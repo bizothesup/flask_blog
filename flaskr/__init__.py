02b3f29ee7c201151c4c319de0f95738dad4be45
babacar sarr <mbayesarr.baba@gmail.com>
2019-09-19 13:49:21 +0000

    regle dinstallation dans les ficher db et init

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask
 
-#create and configure app
-def create_app(test_config =None):
 
+
+# create and configure app
+def create_app(test_config=None):
     """indique à l'application que les
      fichiers de configuration sont relatifs au dossier
       d'instance"""
@@ -12,14 +13,14 @@ def create_app(test_config =None):
     """définit une configuration par 
     défaut que l'application utilisera:"""
     app.config.from_mapping(
-       #est utilisé par Flask et les extensions pour protéger les données.
-        SECRET_KEY = 'dev',
-        #est le chemin où le fichier de base de données SQLite sera enregistré
-        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
+        # est utilisé par Flask et les extensions pour protéger les données.
+        SECRET_KEY='dev',
+        # est le chemin où le fichier de base de données SQLite sera enregistré
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
 
     if test_config is None:
-        #load   the instance config, if  it exist, when not testing
+        # load   the instance config, if  it exist, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
@@ -34,5 +35,9 @@ def create_app(test_config =None):
     @app.route('/hello')
     def hello():
         return 'Hello babs tu es bien installer'
+
+    # init-db a été enregistré avec l'application
+    from . import db
+    db.init_app(app)
 
     return app
--- flaskr/db.py
+++ flaskr/db.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
 """
 current_app est un autre objet spécial qui pointe vers l'application Flask qui gère la demande.
 """
@@ -7,29 +13,28 @@ g est un objet spécial unique pour chaque demande.
  plusieurs fonctions pourraient accéder pendant la requête
 """
 
-import sqlite3
-
-import click
-from flask import current_app, g
-from flask.cli import with_appcontext
 
 
-# get_db() sera appelé lorsque l'application aura été créée et traitera une demande, donc current_app peut être utilisée
+# get_db() sera appelé lorsque l'application aura été créée et
+# traitera une demande, donc current_app peut être utilisée
 def get_db():
     if 'db' not in g:
-        # sqlite3.connect()établit une connexion avec le fichier pointé par la DATABASEclé de configuration
+        # sqlite3.connect()établit une connexion avec
+        # le fichier pointé par la DATABASEclé de configuration
         g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        # sqlite3.Row indique à la connexion de renvoyer des lignes qui se comportent comme des repères.
+        # sqlite3.Row indique à la connexion de
+        # renvoyer des lignes qui se comportent comme des repères.
         g.db.row_factory = sqlite3.Row
     return g.db
 
 
-# close_db() vérifie si une connexion a été créée en vérifiant si g.db était défini.
+# close_db() vérifie si une connexion a été
+# créée en vérifiant si g.db était défini.
 #  Si la connexion existe, elle l'a  ferme.
-def close_bd():
+def close_bd(e=None):
     db = g.pop('db', None)
     if db is not None:
         db.close()
@@ -38,14 +43,24 @@ def close_bd():
 def init_db():
     db = get_db()
 
-    #open_resource()ouvre un fichier relatif au flaskrpackage
+    # open_resource()ouvre un fichier relatif au flaskrpackage
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
-        
-#click.command()définit une commande de ligne de commande
+
+
+# click.command()définit une commande de ligne de commande
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database')
+
+
+# app.teardown_appcontext() indique à Flask d'appeler
+# cette fonction lors du nettoyage après le retour de la réponse.
+# app.cli.add_command() ajoute une nouvelle commande
+# qui peut être appelée avec la flaskcommande.
+def init_app(app):
+    app.teardown_appcontext(close_bd)
+    app.cli.add_command(init_db_command)
