9aa10aa2b434a769817c77271e13a13281708542
babacar sarr <mbayesarr.baba@gmail.com>
2019-09-19 13:29:03 +0000

    creaton de table definit de lection apartir de la ligne de commande

--- flaskr/db.py
+++ flaskr/db.py
@@ -33,3 +33,19 @@ def close_bd():
     db = g.pop('db', None)
     if db is not None:
         db.close()
+
+
+def init_db():
+    db = get_db()
+
+    #open_resource()ouvre un fichier relatif au flaskrpackage
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+        
+#click.command()définit une commande de ligne de commande
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database')
